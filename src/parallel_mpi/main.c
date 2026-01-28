16fa31562841351279b7f0a3fbf051efc7179df7
MattDema <matthew.demarco17@gmail.com>
2026-01-28 23:11:36 +0100

    Structuring the project pt2

---
+++ src/parallel_mpi/main.c
@@ -0,0 +1,23 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) 
+{
+    MPI_Init(&argc, &argv);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    printf("Ciao! Sono il processo MPI %d di %d in esecuzione su %s\n",
+           world_rank, world_size, processor_name);
+
+    MPI_Finalize(); 
+    return 0;
+}
