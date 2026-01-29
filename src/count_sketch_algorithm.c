b96207b471b648d0316a31c5e6ce16cf0ec41727
MattDema <matthew.demarco17@gmail.com>
2026-01-29 23:38:16 +0100

    Fixed cluster's issues (memory leaks due to stack allocation of estimates and strcmp issue)

--- src/count_sketch_algorithm.c
+++ src/count_sketch_algorithm.c
@@ -14,6 +14,16 @@ static uint32_t hash_fnv1a(const char *key, uint32_t seed)
     return hash;
 }
 
+static int compare_ints(const void *a, const void *b) 
+{
+   int32_t arg1 = *(const int32_t *)a;
+    int32_t arg2 = *(const int32_t *)b;
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+}
+
+
 CountSketch* cs_create(int d, int w) 
 {
     CountSketch *cs = (CountSketch *)malloc(sizeof(CountSketch));
@@ -24,7 +34,7 @@ CountSketch* cs_create(int d, int w)
 
     for (int i = 0; i < d; i++) 
     {
-        cs->table[i] = (int32_t *)calloc(w, sizeof(int32_t));
+        cs->table[i] = (int32_t *)calloc(w, sizeof(uint32_t));
     }
 
     cs->seeds = (uint32_t *)malloc(d * sizeof(uint32_t));
@@ -53,12 +63,12 @@ void cs_update(CountSketch *cs, const char *item)
 
 int32_t cs_estimate(CountSketch *cs, const char *item) 
 {
-    int32_t estimates[cs->d];
+    int32_t* estimates = (int32_t *)calloc(cs->d, sizeof(int32_t));
     
     for (int i = 0; i < cs->d; i++) 
     {
-        int32_t h_bucket = hash_fnv1a(item, cs->seeds[i]);
-        int32_t h_sign = hash_fnv1a(item, cs->seeds[i] + 0xDEADBEEF);
+        uint32_t h_bucket = hash_fnv1a(item, cs->seeds[i]);
+        uint32_t h_sign = hash_fnv1a(item, cs->seeds[i] + 0xDEADBEEF);
         
         int bucket = h_bucket % cs->w;
         int sign = (h_sign % 2 == 0) ? 1 : -1;
@@ -67,15 +77,19 @@ int32_t cs_estimate(CountSketch *cs, const char *item)
     }
 
     // Return median estimate
-    qsort(estimates, cs->d, sizeof(int32_t), (int (*)(const void *, const void *))strcmp);
+    qsort(estimates, cs->d, sizeof(int32_t), compare_ints);
+    int32_t results;
     if (cs->d % 2 == 1) 
     {
-        return estimates[cs->d / 2];
+        results = estimates[cs->d / 2];
     } 
     else 
     {
-        return (estimates[(cs->d / 2) - 1] + estimates[cs->d / 2]) / 2;
+        results = (estimates[(cs->d / 2) - 1] + estimates[cs->d / 2]) / 2;
     }
+
+    free(estimates);
+    return results;
 }
 
 void cs_destroy(CountSketch *cs) 
